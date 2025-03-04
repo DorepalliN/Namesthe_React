a017a8f444004b14ce3819bdf6ce237edfbe2cd7
DorepalliN <nagd329@gmail.com>
2025-03-04 19:58:41 +0530

    Setup done for react project

--- app.js
+++ app.js
@@ -1,32 +1,10 @@
 
-// {<div id="parent">
-//     <div id="child1">
-        
-//         <div id="child2">
-
-//             <h1>helloo doing</h1>
-//             <h2>how are you </h2>
-
-//     </div>
-
-// </div>
-
-// </div> }
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 
+const heading= <h1 id="heading">Hello react </h1>
 
+const root=ReactDOM.createRoot(document.getElementById("root"));
 
-const heading=React.createElement("div",{id: "parent"},
-    [React.createElement("div",{id: "child1"},[
-        React.createElement("h1",{},"hello"),
-        React.createElement("h2",{},"hi")]),
-        React.createElement("div",{id: "child2"},[
-            React.createElement("h1",{},"hello"),
-            React.createElement("h2",{},"hi"),]),
-    ]);
-
-    console.log(heading);
-
-    const root=ReactDOM.createRoot(document.getElementById("root"));
-
-    root.render(heading);
+root.render(heading);
